93abd0a7a2285cb0937213d8c91b6f1028264f01
Gabriel Caires <gbrlcaires@gmail.com>
2023-03-07 22:39:25 -0300

    initial commit

---
+++ app/src/main/java/com/devmasterteam/convidados/ui/presents/PresentsViewModel.kt
@@ -0,0 +1,13 @@
+package com.devmasterteam.convidados.ui.presents
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class PresentsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is Presents Fragment"
+    }
+    val text: LiveData<String> = _text
+}
